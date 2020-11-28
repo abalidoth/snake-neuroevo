0b258d3531197403765fa218f793fab6cb9670dc
Gavin Daphne King <gdking01@gmail.com>
2020-11-27 16:41:31 -0800

    added grid runs

---
+++ snake_grid.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pickle
+
+from snake_evo import Population
+
+from tqdm import tqdm
+
+
+PROBS = 2.**np.arange(-2,3) * 0.05
+
+STRENGTHS = 2.**np.arange(-2,3)
+
+GENERATIONS = 200
+
+if __name__ == "__main__":
+    for i_p, p in enumerate(PROBS):
+        for i_s, s in enumerate(STRENGTHS):
+            print(f"testing p={p}, s={s}")
+            max_scores = []
+            P = Population(
+                n_pop = 100,
+                blocks_width=15,
+                blocks_height = 10,
+                hidden_sizes = [10,10]
+            )
+            for i in tqdm(range(GENERATIONS)):
+
+                P.play(n_trials = 5, threshold = 80)
+                max_scores.append(max(P.scores))
+                best = P.generation(num_elites = 2, prob= 0.05, strength=1)
+            with open(f"./grid_runs/{i_p}-{i_s}.pkl", "wb") as f:
+                pickle.dump((p,s,max_scores,P),f)
+
